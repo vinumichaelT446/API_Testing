1d25218eeb70a40ac6a13991d96b9458b0ded411
Vinu Michael <https://github.com/vinumichaelT446/BigBee_Automation>
2026-06-10 08:05:05 +0530

    Deleted flaky files

--- tests/getCall.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Test Get API', async ({ request }) => {
-  const resp = await request.get('https://restful-booker.herokuapp.com/booking');
-  expect(resp.ok()).toBeTruthy();
-  const respJson = await resp.json();
-  console.log(respJson);
-});
-
